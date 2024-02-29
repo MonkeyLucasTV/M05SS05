ed9b65e6fa4549266b874e0a2bcc40c771550982
MonkeyLucasTV <lucas.pokrywa@gmail.com>
2024-02-29 08:49:57 +0100

    Ajout de la visualisation des valeurs du moteur en clair

--- main.cpp
+++ main.cpp
@@ -6,20 +6,26 @@
 
 
 unsigned int ChaineHexaVersInt(char* chaineHexa, int nbCar){
-	int nombre,j = 0;
+	int nombre=0,j = 0;
 	char caractere;
 	for( j=0 ; j<nbCar ; j++ ){
 		caractere = chaineHexa[j];
-		if (caractere >='0' && caractere <= '9') {
+		//std::cout << "Le caractere : " << caractere << std::endl;
+		if(caractere >='0' && caractere <= '9'){
 			nombre = nombre + ((caractere -'0')<<(4*(nbCar-j-1)));
+			//std::cout << "Le nombre : " << nombre << std::endl;
 		}
-		else nombre = nombre + ((caractere - 'A'+10)<<(4*nbCar-j-1));
+		else{
+			nombre = nombre + ((caractere - 'A'+10)<<(4*(nbCar-j-1)));
+			//std::cout << "Le nombre : " << nombre << std::endl;
+		}
 	}
 	return nombre;
 }
 
  int main()
 {
+
 
 	IRClientTCP monclient;
 	char trameCAN[50];
@@ -33,7 +39,9 @@ unsigned int ChaineHexaVersInt(char* chaineHexa, int nbCar){
 			std::cin >> idCAN ;
 			std::cout << "Votre identifiant : " << idCAN << std::endl;
 			std::cout << "Repeter les 2 en boucle ? (Y/N) : ";
-			std::cin >> RepRepT;
+
+			if(Repeter != (true || false)) std::cin >> RepRepT;
+
 			if (RepRepT == "Y") {
 				Repeter = true;
 			}
@@ -45,28 +53,39 @@ unsigned int ChaineHexaVersInt(char* chaineHexa, int nbCar){
 		}
 
 		std::cout << "Successfull" << std::endl;
-		monclient.SeConnecterAUnServeur("172.20.21.26", 4111);
+		monclient.SeConnecterAUnServeur("127.0.0.1", 4111);
 		monclient.Envoyer(idCAN,strlen(idCAN));
 		monclient.Recevoir(trameCAN,50);
 		std::cout << trameCAN << std::endl;
 
 		if (Repeter == true) {
 				while(true){
+
 					strcpy(idCAN, "0B6");
 
-					monclient.SeConnecterAUnServeur("172.20.21.26", 4111);
+					monclient.SeConnecterAUnServeur("127.0.0.1", 4111);
 					monclient.Envoyer(idCAN,strlen(idCAN));
 					monclient.Recevoir(trameCAN,50);
 					std::cout << "Trame Moteur : " << trameCAN << std::endl;
-					Sleep(1200);
+					int VitesseEnKmh =  (ChaineHexaVersInt(trameCAN+23, 2)) * 2.55;
+					std::cout << "Trame vers hexa : " << ChaineHexaVersInt(trameCAN+23, 2) << std::endl;
+					std::cout << "La vitesse : " << VitesseEnKmh << " km/h" << std::endl ;
+
+					int RegimeMoteur = (ChaineHexaVersInt(trameCAN+19,2)) *100 /3.1;
+					std::cout << "Le regime moteur : " << RegimeMoteur << " tr/min" << std::endl << std::endl;
+
+
+					Sleep(500);
+
 
 					strcpy(idCAN, "128");
 
-					monclient.SeConnecterAUnServeur("172.20.21.26", 4111);
+					monclient.SeConnecterAUnServeur("127.0.0.1", 4111);
 					monclient.Envoyer(idCAN,strlen(idCAN));
 					monclient.Recevoir(trameCAN,50);
 					std::cout << "Trame Feux : " << trameCAN << std::endl;
-					Sleep(2000);
+					std::cout << "Trame vers hexa : " << ChaineHexaVersInt(trameCAN+23, 2) << std::endl << std::endl << std::endl;
+					Sleep(5000);
 				}
 		}
 
